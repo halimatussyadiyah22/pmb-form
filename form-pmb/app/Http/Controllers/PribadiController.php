bf675403e34817e2858dd28a2839da4e22007ba8
halimahsyadiyah22 <halimahsyadiyah2202@gmail.com>
2025-02-05 06:58:52 +0700

    register dan login

--- form-pmb/app/Http/Controllers/PribadiController.php
+++ form-pmb/app/Http/Controllers/PribadiController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class PribadiController extends Controller
 {
-    //
+    public function create(){
+        return view('form.pribadi');
+    }
+    public function store(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'nisn' => 'required|min:10|max:10|unique:users',
+            'tl'=>'required|date'
+        ]);
+        $query = User::create([
+            "name" => $request["name"],
+            "nisn" => $request["nisn"],
+            "tl"=>$request["tl"]
+        ]);
+        return redirect('/');
+    }
 }
--- form-pmb/app/Models/Pribadi.php
+++ form-pmb/app/Models/Pribadi.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Pribadi extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'nama_lengkap',
+        'tempat_lahir',
+        'jalan_dusun',
+        'desa_kelurahan',
+        'kecamatan',
+        'kabupaten',
+        'provinsi',
+        'agama',
+        'no_kk',
+        'email',
+        'status',
+        'no_wa',
+        'kewarganegaraan',
+        'jurusan'
+    ];
 }
